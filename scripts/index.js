5f19181f13a880afa508a077b7c754ba95942e3b
verymarine <thedoroshenkomarina@gmail.com>
2021-10-26 23:55:49 +0300

    проект 5 на проверку

--- scripts/index.js
+++ scripts/index.js
@@ -1,195 +1,177 @@
 // определяю переменные для всплывающего окна редактирования профиля
-const editButton = document.querySelector('.profile__button');
-const popupCloseButton = document.querySelector('.popup__close_type_edit');
+const profile = document.querySelector(".profile");
+const editButton = profile.querySelector(".profile__button");
 
 // создаю переменную для инпута (который меняет имя)
-const nameField = document.querySelector('.popup__input_type_name')
-const jobField = document.querySelector('.popup__input_type_job')
-const profileName =document.querySelector('.profile__name');
-const profileJob = document.querySelector('.profile__job');
-const popupForm = document.querySelector('.popup__form_type_edit');
+const nameField = document.querySelector(".popup__input_type_name");
+const jobField = document.querySelector(".popup__input_type_job");
+const profileName = profile.querySelector(".profile__name");
+const profileJob = profile.querySelector(".profile__job");
+const popupForm = document.querySelector(".popup__form_type_edit");
+
+//кнопка-крестик у поп-ап
+const popupCloseButton = document.querySelector(".popup__close_type_edit");
 
 //  присвоила переменную для поп-апа Редактировать профиль
-const popupEditProfile = document.querySelector('.popup_edit');
-
-//переменная для использование карточек мест!!!!!!!!!!!
-const contentPhoto = document.querySelector('.content');
-
-// переменная темплет для распознавания контента внутри тега
-const templateItem = document.querySelector('.template').content;
+const popupEditProfile = document.querySelector(".popup_edit");
 
 // переменная для формы добавления Нового места
-const formAddPlace = document.querySelector('.popup__form_type_add-place');
+const formAddPlace = document.querySelector(".popup__form_type_add-place");
 
 //переменные для значений Места и ссылки картинки
-const placeField = document.querySelector('.popup__input_type_place');
-const linkField = document.querySelector('.popup__input_type_link'); 
+const placeField = document.querySelector(".popup__input_type_place");
+const linkField = document.querySelector(".popup__input_type_link");
 
 //присвоила переменную для поп-апа Новое место
-const popupAddPlace = document.querySelector('.popup_add');
+const popupAddPlace = document.querySelector(".popup_add");
 
 // переменные для кнопки добавить новое место
-const buttonAddPlace = document.querySelector('.profile__add-button');
+const buttonAddPlace = profile.querySelector(".profile__add-button");
+
 // переменная для кнопки-крестика поп-ап Новое место
-const buttonCloseAddPlace = document.querySelector('.popup__close_type_add-place');
+const buttonCloseAddPlace = document.querySelector(".popup__close_type_add-place");
 
+//переменная для использование карточек мест
+const contentPhoto = document.querySelector(".content");
 
-const popupBigImage = document.querySelector('.popup_picture');
-const popupImage = document.querySelector('.popup__image');
-const popupCapture = document.querySelector('.popup__capture');
-const buttonCloseImage = document.querySelector('.popup__close_type_big-image');
+// переменная темплет для распознавания контента внутри тега
+const templateItem = document.querySelector(".template").content;
 
-// ф-ии которые открывают большую картинку
-// function openBigImage() {
-//   popupBigImage.classList.add('popup_opened');
-//   popupImage.src = article.querySelector('.content__image').src;
-// };
+// переменные для сосздания поп-ап большой картинки
+const popupBigImage = document.querySelector(".popup_picture");
+const popupImage = document.querySelector(".popup__image");
+const popupCapture = document.querySelector(".popup__capture");
 
-function closeBigImage() {
-  popupBigImage.classList.remove('popup_opened');
+// добавления кнопки-крестика для большой картинки
+const buttonCloseImage = document.querySelector(".popup__close_type_big-image");
+
+//ф-я которая открывает поп-ап
+function openPopup(popup) {
+  popup.classList.add("popup_opened");
 }
-
 
 // добавила ф-ю которая закрывает поп-ап
-function closePopup() {
-  popupEditProfile.classList.remove('popup_opened');
-  
+function closePopup(popup) {
+  popup.classList.remove("popup_opened");
 }
 
-// // добавила ф-ю которая открывает поп-ап
+// ф-я закрытия поп-ап Редактирования профиля
+function closeEditProfile() {
+  closePopup(popupEditProfile);
+}
 
-function openPopup() {
-  popupEditProfile.classList.add('popup_opened');
+// ф-я при открытии поп-ап в формах указаны значения из профиля
+function valueEditInput() {
+  openPopup(popupEditProfile);
   jobField.value = profileJob.textContent;
   nameField.value = profileName.textContent;
 }
 
-// изменения текста при помощи ф-ии 
-
-function submitForm(event) {
-  event.preventDefault()
-
+// изменения текста при помощи ф-ии
+function handleEditFormSubmit(event) {
+  event.preventDefault();
   profileName.textContent = nameField.value;
   profileJob.textContent = jobField.value;
 
-  closePopup();
+  closePopup(popupEditProfile);
 }
 
-// массив с объектами Карточек
-const items = [
-  {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }
-];
-
-// ф-я открытия  поп-ап Новое место 
+// ф-я открытия  поп-ап Новое место
 function openPopupAddPlace() {
-  popupAddPlace.classList.add('popup_opened');
-};
+  openPopup(popupAddPlace);
+}
 
-// ф-я закрытия поп-ап новое место
 function closePopupAddPlace() {
-  popupAddPlace.classList.remove('popup_opened');
-};
+  closePopup(popupAddPlace);
+}
 
+//удаление карточки Места
+function handleDeleteCard(evt) {
+  const deleteElement = evt.target;
+  deleteElement.closest(".content__grid").remove();
+}
 
+//добавление лайка
+function handleLikeActive(evt) {
+  const likeElement = evt.target;
+  likeElement.classList.toggle("content__liked");
+}
 
+// ф-я открытия большой картинки
+function openBigImage(event) {
+  openPopup(popupBigImage);
 
-items.forEach(prependContentBlock);
+  const card = event.target.closest(".content__grid");
+  popupImage.src = card.querySelector(".content__image").src;
+  popupCapture.innerText = card.querySelector(".content__title").innerText;
+  popupImage.alt = card.querySelector(".content__title").innerText;
+}
+
+function closeBigImage() {
+  closePopup(popupBigImage);
+}
+
+// ф-я
+function setEventListener(card) {
+  card
+    .querySelector(".content__trashcan")
+    .addEventListener("click", handleDeleteCard);
+  card
+    .querySelector(".content__like")
+    .addEventListener("click", handleLikeActive);
+  card.querySelector(".content__image").addEventListener("click", openBigImage);
+}
 
 // ф-я для вставления блоков, с возможностью удаления блоков и возможностью ставить и убирать лайки у отдельных блоков
 function createContentBlock(item) {
-  const article = templateItem.querySelector('.content__grid').cloneNode(true);
-  article.querySelector('.content__image').src = item.link;
-  article.querySelector('.content__title').innerText = item.name;
-  article.querySelector('.content__image').alt = item.name;
+  const card = templateItem.querySelector(".content__grid").cloneNode(true);
+  card.querySelector(".content__image").src = item.link;
+  card.querySelector(".content__title").innerText = item.name;
+  card.querySelector(".content__image").alt = item.name;
 
-  article.querySelector('.content__image').addEventListener('click', openBigImage);
+  setEventListener(card);
+  return card;
+}
 
-  article.querySelector('.content__trashcan').addEventListener('click',(event)=> {
-  article.remove();
-  });
-
-  article.querySelector('.content__like').addEventListener('click',(event)=> {
-    event.target.classList.toggle('content__liked');
-    });
-    function openBigImage() {
-      popupBigImage.classList.add('popup_opened');
-      popupImage.src = article.querySelector('.content__image').src;
-      popupCapture.innerText = article.querySelector('.content__title').innerText;
-      popupImage.alt = article.querySelector('.content__title').innerText;
-    };
-
-  return article;
-};
-
-// ф-я добавления блоков в начало 
+// ф-я добавления блоков в начало
 function prependContentBlock(item) {
-  const article = createContentBlock(item);
-  contentPhoto.prepend(article) 
-};
+  const card = createContentBlock(item);
+  contentPhoto.prepend(card);
+}
 
 //ф-я отправления внесенных значений с дальнейшим закрытием и сбросом инфо в поп-ап
-function addPlaceSubmit(event){
+function addPlaceSubmit(event) {
   event.preventDefault();
   const name = placeField.value;
   const link = linkField.value;
-
   const card = {
     name: name,
-    link: link
-  }
+    link: link,
+  };
 
   prependContentBlock(card);
-  closePopupAddPlace();
+  closePopup(popupAddPlace);
   event.target.reset();
-};
+}
 
-
-// слушатели 
-formAddPlace.addEventListener('submit', addPlaceSubmit);
-buttonAddPlace.addEventListener('click', openPopupAddPlace);
-buttonCloseAddPlace.addEventListener('click', closePopupAddPlace);
+items.forEach(prependContentBlock);
 
 // слушатель по клику которого открывает поп-ап
-editButton.addEventListener('click', openPopup)
+editButton.addEventListener("click", valueEditInput);
 
 // слушатель по клику к-го закрывается поп-ап (нажатие на крестик)
-popupCloseButton.addEventListener('click', closePopup)
+popupCloseButton.addEventListener("click", closeEditProfile);
 
 // добавили слушателя клику сохранить/enter задача закрыть поп-ап и поменять значения
-popupForm.addEventListener('submit', submitForm)
+popupForm.addEventListener("submit", handleEditFormSubmit);
 
-buttonCloseImage.addEventListener('click', closeBigImage)
+// слушатели  для добавления и удаления места
+formAddPlace.addEventListener("submit", addPlaceSubmit);
+buttonAddPlace.addEventListener("click", openPopupAddPlace);
+buttonCloseAddPlace.addEventListener("click", closePopupAddPlace);
 
-// function popupClickHandler(event) {
+// слушатель для кнопки-крестика большой картинки
+buttonCloseImage.addEventListener("click", closeBigImage);
 
-//   if (event.target.classList.contains('popup')) {
-  
-//   closePopup()
-// }
-// } 
-
-// слушатель по клику к-го закрывается поп-ап (нажатие на любое место)
-// popup.addEventListener('mouseup', popupClickHandler)
+// слушатель для закрытия большой картинки
+buttonCloseImage.addEventListener("click", closeBigImage);
